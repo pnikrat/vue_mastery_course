b72a47d9cf6755f3b6c854b6bc7d6e15e22d106f
Przemysław Nikratowicz <pnikratowicz@prograils.com>
2019-03-12 17:03:23 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'These are white socks',
+    image: './assets/images/vm_socks_green.jpg',
+    imageAlt: 'The green socks',
+    link: 'https://www.google.com',
+  }
+});
