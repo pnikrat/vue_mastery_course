55ff45849cd2d0ad55c5caec19b31490e0e26017
Przemysław Nikratowicz <pnikratowicz@prograils.com>
2019-03-15 16:25:55 +0100

    Computed properties

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
     description: 'These are white socks',
-    image: './assets/images/vm_socks_green.jpg',
+    selectedVariant: 0,
     imageAlt: 'The green socks',
     link: 'https://www.google.com',
-    inStock: true,
-    onSale: true,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 256,
         variantColor: 'green',
         variantImage: './assets/images/vm_socks_green.jpg',
+        variantQuantity: 10,
+        onSale: true,
       },
       {
         variantId: 266,
         variantColor: 'blue',
         variantImage: './assets/images/vm_socks_blue.jpg',
+        variantQuantity: 0,
+        onSale: false,
       }
     ],
     sizes: [37, 38, 39, 40, 41, 42, 43, 44, 45],
@@ -31,8 +34,26 @@ var app = new Vue({
     removeFromCart() {
       if (this.cart > 0) this.cart -= 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+      console.log(index);
     },
-  }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    },
+    onSale() {
+      return this.variants[this.selectedVariant].onSale
+    },
+    saleNotification() {
+      return this.title + ' is on sale right now!';
+    },
+  },
 });
