e20ecf9832e7874527e96527b394a7a3663b617e
Przemysław Nikratowicz <pnikratowicz@prograils.com>
2019-03-16 11:24:53 +0100

    Review form

--- main.js
+++ main.js
@@ -40,11 +40,25 @@ Vue.component('product', {
               Add to Cart
           </button>
           <button @click="removeFromCart">Remove from Cart</button>
-          <div class="cart">
-            <p>Cart({{ cart }})</p>
-          </div>
         </div>
       </div>
+
+      <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+          <li v-for="review in reviews">
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+            <p v-if="review.recommendation">Would recommend the product</p>
+            <p v-else>Would not recommend the product</p>
+          </li>
+        </ul>
+      </div>
+
+      <product-review @review-submitted="addReview"></product-review>
+
     </div>
   `,
   data() {
@@ -73,18 +87,21 @@ Vue.component('product', {
         }
       ],
       sizes: [37, 38, 39, 40, 41, 42, 43, 44, 45],
-      cart: 0,
+      reviews: [],
     };
   },
   methods: {
     addToCart() {
-      this.cart += 1;
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
     },
     removeFromCart() {
-      if (this.cart > 0) this.cart -= 1;
+      this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId);
     },
     updateProduct(index) {
       this.selectedVariant = index;
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview);
     },
   },
   computed: {
@@ -125,11 +142,98 @@ Vue.component('detail', {
   `,
 });
 
+Vue.component('product-review', {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name">
+      </p>
+
+      <p>
+        <label for="review">Review:</label>
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+
+      <p>
+        <p>Would you recommend this product?<p/>
+        <label><input type="radio" v-model="recommendation" :value="true">Yes</label>
+        <label><input type="radio" v-model="recommendation" :value="false">No</label>
+      </p>
+
+      <p>
+        <input type="submit" value="Submit">
+      </p>
+    </form>
+
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      recommendation: null,
+      errors: [],
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.errors = [];
+      if (this.name && this.review && this.rating && this.recommendation !== null) {
+        const productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+          recommendation: this.recommendation
+        };
+        this.$emit('review-submitted', productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+        this.recommendation = null;
+      } else {
+        if (!this.name) this.errors.push('Name required.');
+        if (!this.review) this.errors.push('Review required.');
+        if (!this.rating) this.errors.push('Rating required.');
+        if (this.recommendation === null) this.errors.push('Recommendation required.');
+      }
+    },
+  },
+});
 
 var app = new Vue({
   el: '#app',
   data: {
     premium: true,
+    cart: [],
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    },
+    removeFromCart(id) {
+      var index = this.cart.indexOf(id);
+      if (index > -1) this.cart.splice(index, 1);
+    },
   },
 });
 
