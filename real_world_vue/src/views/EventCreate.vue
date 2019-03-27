a51d562dbba22bbf011751ca8fd8229a12a1ba80
Przemysław Nikratowicz <pnikratowicz@prograils.com>
2019-03-27 11:17:16 +0100

    Adding Vuex

--- real_world_vue/src/views/EventCreate.vue
+++ real_world_vue/src/views/EventCreate.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Create event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ categoriesLength }} categories</p>
+    <ul>
+      <li v-for="category in categories" :key="category">{{ category }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    categoriesLength() {
+      return this.$store.getters.categoriesLength;
+    },
+    ...mapState(['user', 'categories']),
+  },
+};
+</script>
